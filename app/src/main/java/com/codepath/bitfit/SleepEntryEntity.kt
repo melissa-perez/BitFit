96c8d52a4f76d0690ae2eb0108582cbfab532d32
Melissa Perez <melmperz@gmail.com>
2023-03-26 18:11:17 -0400

    Create initial entity and update build.gradle

---
+++ app/src/main/java/com/codepath/bitfit/SleepEntryEntity.kt
@@ -0,0 +1,15 @@
+package com.codepath.bitfit
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.Date
+
+@Entity(tableName = "sleep_entry_table")
+data class SleepEntryEntity(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    @ColumnInfo(name = "sleptHours") val sleptHours: Float,
+    @ColumnInfo(name = "feelingRating") val feelingRating: Int,
+    @ColumnInfo(name = "sleepNotes") val sleepNotes: String?,
+    @ColumnInfo(name = "sleepDate") val sleepDate: Date,
+)
