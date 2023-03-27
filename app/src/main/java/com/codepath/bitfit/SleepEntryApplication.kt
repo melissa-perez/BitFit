a93247f98e6a177646e4e7dca591c7c5af8ab154
Melissa Perez <melmperz@gmail.com>
2023-03-27 18:50:59 -0400

    Work on RecyclerAdapter code

--- app/src/main/java/com/codepath/bitfit/SleepEntryApplication.kt
+++ app/src/main/java/com/codepath/bitfit/SleepEntryApplication.kt
@@ -1,7 +1,6 @@
 package com.codepath.bitfit
 
 import android.app.Application
-import androidx.core.content.ContentProviderCompat.requireContext
 
 class SleepEntryApplication : Application() {
     val db by lazy { AppDatabase.getInstance(this) }
